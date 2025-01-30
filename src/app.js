a85b7b30e296a66f87a9569fada721217f8027b5
harshgurehiya <harsh.gurehiya@gmail.com>
2025-01-30 18:35:01 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Test response!");
-});
+// app.get(/.*fly$/, (req, res) => {
+//   res.send({ firstName: "Harsh", lastName: "Gurehiya" });
+// });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello world!");
-});
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+//   res.send({ firstName: "Harsh", lastName: "Gurehiya" });
+// });
 
-app.use("/", (req, res) => {
-  res.send("Hello from server!");
+//Dynamic Routing..
+app.get("/user/:userID/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Harsh", lastName: "Gurehiya" });
 });
 
 app.listen(7777, () => {
-  console.log("Server successfully started!");
+  console.log("Server successfully started...");
 });
