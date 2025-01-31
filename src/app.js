2f8a98eea1f21e00a7b034496206df4fc2a3663f
harshgurehiya <harsh.gurehiya@gmail.com>
2025-01-31 20:14:34 +0530

    Middlewares

--- src/app.js
+++ src/app.js
@@ -2,20 +2,29 @@ const express = require("express");
 
 const app = express();
 
-// app.get(/.*fly$/, (req, res) => {
-//   res.send({ firstName: "Harsh", lastName: "Gurehiya" });
-// });
-
-// app.get("/user", (req, res) => {
-//   console.log(req.query);
-//   res.send({ firstName: "Harsh", lastName: "Gurehiya" });
-// });
-
-//Dynamic Routing..
-app.get("/user/:userID/:name/:password", (req, res) => {
+app.get("/user/:userID/:name", (req, res, next) => {
+  console.log("Handling Response..");
   console.log(req.params);
-  res.send({ firstName: "Harsh", lastName: "Gurehiya" });
+  //res.send("Response!!");
+  next();
 });
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling route handler 1");
+    //res.send("Response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling route handler 2");
+    res.send("Response 2");
+  },
+  (req, res, next) => {
+    console.log("Handling route handler 3");
+    res.send("Response 3");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server successfully started...");
